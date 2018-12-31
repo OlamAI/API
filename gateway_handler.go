b9e87b23c967eaff30977d43266ee49fdbb4c4de
Zac Holland <zacharyholland@gmail.com>
2018-12-31 13:53:19 -0800

    removed handler code

--- gateway_handler.go
+++ gateway_handler.go
@@ -1,17 +1,17 @@
-package api
+// package api
 
-import (
-	"log"
+// import (
+// 	"log"
 
-	"golang.org/x/net/context"
-)
+// 	"golang.org/x/net/context"
+// )
 
-// Server represents the gRPC server
-type Server struct {
-}
+// // Server represents the gRPC server
+// type Server struct {
+// }
 
-// SayHello generates response to a Ping request
-func (s *Server) SayHello(ctx context.Context, in *PingMessage) (*PingMessage, error) {
-	log.Printf("Receive message %s", in.Greeting)
-	return &PingMessage{Greeting: "bar"}, nil
-}
+// // SayHello generates response to a Ping request
+// func (s *Server) SayHello(ctx context.Context, in *PingMessage) (*PingMessage, error) {
+// 	log.Printf("Receive message %s", in.Greeting)
+// 	return &PingMessage{Greeting: "bar"}, nil
+// }
